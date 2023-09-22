b4ced9f6a3feda443dfe6ad559ff900c6a7274be
vicmaburrito <vmaldaraca@gmail.com>
2023-09-22 01:00:08 -0600

    Add Navbar, Home components and FirstBlock

---
+++ src/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Header';
+import FirstBlock from '../FirstBlock';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <FirstBlock />
+    </>
+  );
+}
