7b73e094bb0670075fa67a33b708840d9dd2cce8
vicmaburrito <vmaldaraca@gmail.com>
2023-09-22 22:39:59 -0600

    Fix eslint errors

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from '../Header';
 import FirstBlock from '../FirstBlock';
-import { Routes, Route } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route index path='/' element={<FirstBlock />} />
+        <Route index path="/" element={<FirstBlock />} />
         {/* <Route path='/boleta' element={<Boleta />} /> */}
         {/* <Route path='/maestros' element={<Maestros />} /> */}
         {/* <Route path='/perfil' element={<Perfil />} /> */}
