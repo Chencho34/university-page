8d936b2bb3e49b1fd1e60c7699fb163707091067
Armando <armandocrescencio343@gmail.com>
2023-09-22 18:52:10 -0600

    add react-router-dom

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from '../Header';
 import FirstBlock from '../FirstBlock';
+import { Routes, Route } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <FirstBlock />
+      <Routes>
+        <Route index path='/' element={<FirstBlock />} />
+        {/* <Route path='/boleta' element={<Boleta />} /> */}
+        {/* <Route path='/maestros' element={<Maestros />} /> */}
+        {/* <Route path='/perfil' element={<Perfil />} /> */}
+      </Routes>
     </>
   );
 }
